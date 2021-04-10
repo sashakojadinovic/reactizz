dc315a1cc52bd2b3fc1942141b4c63faed5c20f3
sashakojadinovic <stanojeglavas@msn.com>
2021-04-10 11:53:36 +0200

    test33

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -10,7 +10,7 @@ function Answer(props){
         additionalClasses += 'red';
     }
     return (
-        //ovo ćeš verovatno da skloniš
+        //ovo ćeš verovatno da skloniš ako mobile buttons budu wide po defaultu
         <div className={`col2 ${props.answer.text.length>17?"flex-wide":""}`}>
             {props.finished && props.answer.points>0 && props.answer.answered?<p className="answer-points">+{props.answer.points}</p>:''}
             {props.finished && props.answer.points<0 && props.answer.answered?<p className="answer-points">{props.answer.points}</p>:''}
